dbcff56e7aa483bfa55d218b6bc78dc262609f6d
lumi <lumi.mia.kalt@gmail.com>
2026-01-25 22:57:50 +0000

    sops git fixed question mark

--- modules/sops.nix
+++ modules/sops.nix
@@ -10,6 +10,7 @@
 
       secrets."ssh/github/private_key" = {
         owner = "lumi";
+        group = "users";
         mode = "0600";
         path = "${home}/.ssh/id_ed25519";
         neededForUsers = true;
