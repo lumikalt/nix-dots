cff538b5a35855c150222a789461bbf54f74006c
lumi <lumi.mia.kalt@gmail.com>
2026-02-07 18:26:44 +0000

    IME setup

--- modules/language.nix
+++ modules/language.nix
@@ -1,5 +1,23 @@
 { pkgs, ... }:
 {
+  i18n.inputMethod = {
+    type = "fcitx5";
+    enable = true;
+
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+      fcitx5-mellow-themes
+    ];
+  };
+
+  # Should be automatic, but let's be explicit
+  environment.sessionVariables = {
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+  };
+
   home-manager.users.lumi = {
     programs.anki = {
       enable = true;
@@ -8,5 +26,9 @@
         ankiAddons.review-heatmap
       ];
     };
+
+    home.packages = with pkgs; [
+      qt6Packages.fcitx5-configtool
+    ];
   };
 }
