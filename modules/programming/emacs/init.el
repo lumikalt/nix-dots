968854ecdedd45e609eb82f889a9dc007dcf73bd
lumi <lumi.mia.kalt@gmail.com>
2026-03-12 23:45:42 +0000

    Outdated flag

--- modules/programming/emacs/init.el
+++ modules/programming/emacs/init.el
@@ -153,7 +153,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty +roam2 +present +hugo)               ; organize your plain life in plain text
+       (org +pretty +roam +present +hugo)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
