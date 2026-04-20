94216d8ce1c06c716b95d233367b4e92966ec39e
lumi <lumi.mia.kalt@gmail.com>
2026-04-20 17:35:24 +0100

    Virtbox

--- modules/virt.nix
+++ modules/virt.nix
@@ -2,6 +2,10 @@
 {
   virtualisation.docker.enable = true;
 
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  users.extraGroups.vboxusers.members = [ "lumi" ];
+
   home-manager.users.lumi = {
     home.packages = with pkgs; [
       docker
