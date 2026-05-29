2e37ab295bf2737274d45c961c4638e78babfea4
lumi <lumi.mia.kalt@gmail.com>
2026-05-29 17:51:05 +0100

    Stoat requires an ancient electron... bad

--- modules/chatting.nix
+++ modules/chatting.nix
@@ -2,7 +2,7 @@
 {
   home-manager.users.lumi = {
     home.packages = with pkgs; [
-      stoat-desktop
+      # stoat-desktop # why does this require an outdated chromium
       signal-desktop
       wasistlos # whatsapp
       gajim # xmpp
