e92b45da19cf9d26771c1bdff62ff7bd8e595295
lumi <lumi.mia.kalt@gmail.com>
2026-05-29 18:06:17 +0100

    No need for a Whatsapp client

--- modules/chatting.nix
+++ modules/chatting.nix
@@ -2,10 +2,8 @@
 {
   home-manager.users.lumi = {
     home.packages = with pkgs; [
-      # stoat-desktop # why does this require an outdated chromium
+      # stoat-desktop # outdated chromium
       signal-desktop
-      wasistlos # whatsapp
-      gajim # xmpp
     ];
 
     programs.vesktop = {
